358a16b5df314cbabdd1932366e53fe9c38c69e2
Dmitry Drozd <drozd.dzmitryi@gmail.com>
2017-02-02 20:56:28 +0300

    Added test logycs

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.lastGuess = this.lastGuess ? this.lastGuess : Math.ceil(this.min + (this.max - this.min) / 2);
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.lastGuess = Math.ceil(this.min + (this.lastGuess - this.min) / 2);
+        this.guess();
     }
 
     greater() {
-
+        this.lastGuess = Math.ceil(this.max - (this.max - this.lastGuess) / 2);
+        this.guess();
     }
 }
 
